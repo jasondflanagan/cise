f9dd75edcadde88767262ff26db5fb8585e3f47d
Jason Flanagan <jasondflanagan1@outlook.com>
2021-10-22 17:16:16 -0400

    photos sorted

--- photography.php
+++ photography.php
@@ -36,7 +36,17 @@
             </div>
             <div>
                 <ul>
-
+                <?php
+		$dirname = "images/";
+		$images = scandir($dirname);
+		shuffle($images);
+		$ignore = array(".", "..");
+		foreach($images as $curimg){
+			if(!in_array($curimg, $ignore)) {
+				echo "<li><a href=\"$dirname$curimg\"><img src='img.php?src=$dirname$curimg&w=300&zc=1' alt='' /></a></li>\n ";
+			}
+		} 				
+	?>
                 </ul>
                 
             </div>
